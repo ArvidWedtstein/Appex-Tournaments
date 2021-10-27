054a635ac2820ef58b99564806927a2c38a2bfe9
ArvidW <arvidw0310@gmail.com>
2021-10-27 10:37:40 +0200

    vueee

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <!-- Remove this component to get started! -->
-    <NuxtWelcome />
+    <button class="newTournament" type="button">New Tourament</button>
   </div>
 </template>
