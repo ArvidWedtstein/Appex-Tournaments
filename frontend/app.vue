6eb02b5b097da64dc01bdcb30a7045ad79eb30f8
ArvidW <arvidw0310@gmail.com>
2021-10-27 12:22:13 +0200

    hmmmmm

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,3 +1,4 @@
+
 <template>
   <div>
     <button class="newTournament" type="button">New Tourament</button>
@@ -8,94 +9,163 @@
       <input type="text" id="tdate" name="tdate" placeholder="Date"><br>
       <label for="tplayers">Players:</label><br>
       <textarea id="tplayers" name="tplayers" rows="4" cols="50"></textarea><br><br>
-      <input type="submit" value="Submit">
+      <button class="newTournament" v-on:click="newTournament()" type="button">New Tourament</button>
+      <!--<input type="submit" value="Submit">-->
     </form>
-    <div class="wrapper">
-      <div class="item">
-        <div class="item-parent">
-          <p>Parent2</p>
+    <div class="tournament-brackets">
+        <div class="bracket">
+            <template v-for="(round, index) in rounds" :key="index">
+                <div class="round" :class="['round-' + round]">
+                    <template v-for="(match, matchIndex) in round" :key="matchIndex">
+                        <div class="match"><div class="match__content" :style="matchStyle"></div></div>
+                    </template>
+                </div>
+            </template>
         </div>
-        <div class="item-childrens">
-          <div class="item-child">
-            <div class="item">
-              <div class="item-parent">
-                <p>Parent3</p>
-              </div>
-              <div class="item-childrens">
-                <div class="item-child">
-                  <div class="item">
-                    <div class="item-parent">
-                      <p>Parent</p>
-                    </div>
-                    <div class="item-childrens">
-                      <div class="item-child">
-                        <p>childfirst 1</p>
-                      </div>
-                      <div class="item-child">
-                        <p>childfirst 2</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="item-child">
-                  <p>child 2</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="item-child">
-            <div class="item">
-              <div class="item-parent">
-                <p>Parent</p>
-              </div>
-              <div class="item-childrens">
-                <div class="item-child">
-                  <div class="item">
-                    <div class="item-parent">
-                      <p>Parent</p>
-                    </div>
-                    <div class="item-childrens">
-                      <div class="item-child">
-                        <p>child 1</p>
-                      </div>
-                      <div class="item-child">
-                        <p>child 2</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="item-child">
-                  <p>child 2</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
 
-
 <script>
+const defaultRounds = [256, 128, 64, 32, 16, 8, 4, 2, 1]
+/*
+      bracketSize: {
+        type: Number,
+        default: 2
+      },
+      */
 export default {
     name: "Tournaments",
-    data() {
-      return {
-        messages: null,
-        dark: false,
-        showMessage: true,
-        msgindex: 0
+    props: {
+      matchStyle: {
+        type: Object,
+        default: () => ({
+          border: '2px solid black',
+          width: '100%',
+          height: '30px',
+          position: 'relative'
+        })
       }
     },
-    asyncData () {
-
+    data() {
+      return {
+        players: [],
+        bracketSize: 0
+      }
     },
     methods: {
-      
+      newTournament() {
+        const field = document.getElementById('tplayers').value;
+        let players = field.split("\n");
+        console.log(players)
+        this.players = players;
+        this.bracketSize = players.length;
+        console.log(this.bracketSize)
+        this.rounds();
+      },
+      rounds () {
+        return defaultRounds.filter(rounds => rounds <= this.bracketSize)
+      }
     },
     mounted() {
+
+    },
+    computed: {
       
     }
 }
 </script>
+
+
+
+<style>
+    *,
+    *::after,
+    *::before {
+        box-sizing: border-box;
+    }
+
+    .bracket {
+        display: flex;
+    }
+
+    .round {
+        display: flex;
+        flex-grow: 1;
+        flex-direction: column;
+    }
+
+    .round:first-child .match::before {
+        display: none;
+    }
+
+    .round:first-child .match__content::before {
+        display: none;
+    }
+
+    .round:last-child .match::after {
+        display: none;
+    }
+
+    .match {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        margin: 0 10px;
+        padding: 6px 0;
+        flex-grow: 1;
+        position: relative;
+    }
+
+    .match::before {
+        content: "";
+        display: block;
+        min-height: 30px;
+        border-left: 2px solid purple;
+        position: absolute;
+        left: -10px;
+        top: 50%;
+        margin-top: -15px;
+        margin-left: -2px;
+    }
+
+    .match:nth-child(odd)::after {
+        content: "";
+        display: block;
+        border: 2px solid transparent;
+        border-top-color: purple;
+        border-right-color: purple;
+        height: 50%;
+        position: absolute;
+        right: -10px;
+        width: 10px;
+        top: 50%;
+    }
+
+    .match:nth-child(even)::after {
+        content: "";
+        display: block;
+        border: 2px solid transparent;
+        border-bottom-color: purple;
+        border-right-color: purple;
+        height: 50%;
+        position: absolute;
+        right: -10px;
+        width: 10px;
+        bottom: 50%;
+    }
+
+    .match__content::before {
+        content: "";
+        display: block;
+        width: 10px;
+        border-bottom: 2px solid purple;
+        margin-left: -2px;
+        position: absolute;
+        top: 50%;
+        left: -10px;
+    }
+</style>
+
+
+
