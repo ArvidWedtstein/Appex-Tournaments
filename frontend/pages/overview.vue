5c23ba832ce7bb34f02feee2df58d16aadf85544
ArvidW <71834553+ArvidWedtstein@users.noreply.github.com>
2021-11-01 12:44:01 +0100

    system for å overføre vinner til neste runde

--- frontend/pages/overview.vue
+++ frontend/pages/overview.vue
@@ -6,7 +6,7 @@
       <form>
         <div class="inputBox">
           <h2>Skriv inn navnet på tournamentet</h2>
-          <input v-model.trim="tournamentName" type="text" id="tname" name="tname" placeholder="Tournament navn" maxlength = "69">
+          <input v-model="tournamentName" type="text" id="tname" name="tname" placeholder="Tournament navn" maxlength = "69">
           <span class="limiter">{{ 69 - tournamentName.length }} characters remaining</span>
         </div>
         <div class="inputBox">
@@ -28,14 +28,13 @@
       <form>
         <h1 class="title">{{tournamentName}} deltakere</h1>
         <div v-for="index in playerInt" :key="index" class="deltakere">
-            <input type="text" v-model="players[index]" v-bind:placeholder="`player${index}`">
+            <input v-bind:id="index" type="text" :v-model="'matches['+index +']'" v-bind:placeholder="'player' + index">
         </div>
         <textarea id="tplayers" name="tplayers" rows="4" cols="50"></textarea><br><br>
         <button class="newTournament" v-on:click="newTournament()" type="button">New Tourament</button>
         <!--<input type="submit" value="Submit">-->
         <div class="pagebtn">
           <button class="past" v-on:click="pageSwitch('past')"><i class="fas fa-arrow-left"></i></button>
-          <button class="next" v-on:click="pageSwitch('next')"><i class="fas fa-arrow-right"></i></button>
         </div>
       </form>
     </div>
@@ -46,7 +45,7 @@
               <div class="match" v-for="match in round" :key="match">
                   <div class="match__content"></div>
                       <div class="matchplayer" v-for="player in match" :key="player">
-                          <button class="player" v-on:click="matchWin(player)" type="button">{{player}}</button>
+                          <button class="player" v-on:click="matchWin(round, player)" type="button">{{player}}</button>
                       </div>
                   </div>
               </div>
@@ -67,56 +66,86 @@
     </div>
   </div>
 </template>
-
+    
 <script>
 const defaultRounds = [256, 128, 64, 32, 16, 8, 4, 2, 1];
 export default {
     name: "Tournaments",
     data() {
       return {
-        page: 0,
-        tournamentName: '',
+        page: 1,
+        tournamentName: 'test',
         players: 0,
-        playerInt: 0,
+        playerInt: 8,
         bracketSize: 0,
         matches: [
           [
-            ['player1', 'player2'],
-            ['player12', 'player22']
+
           ],
           [
-            ['winner1', "winner2"]
+
           ],
           [
-              ['final winner']
+
           ]
         ]
       }
     },
     methods: {
       newTournament() {
+        this.playerInt = parseInt(this.playerInt);
         const field = document.getElementById("tplayers").value;
         this.players = field.split("\n").length;
-        console.log(this.players)
+        let playerlist = field.split("\n");
+        console.log(playerlist.length)
+        let matchlist = [];
+        for (let i = 0; i < this.playerInt; i+=2) {
+            matchlist.push(playerlist[i])
+            matchlist.push(playerlist[i+1])
+            this.matches[0].push(matchlist);
+            matchlist = []
+        }
+        console.table(this.matches)
         this.bracketSize = this.players.length;
-        console.log(this.bracketSize)
-
+        let nextmatchint = this.matches[0].length / 2;
+        for (let i = 0; i < nextmatchint; i++) {
+          this.matches[1].push([])
+          
+        }
+        this.matches[2].push([])
+        this.pageSwitch('next');
       },
-      matchWin(value) {
+      matchWin(round, playername) {
         //const playerfield = document.getElementByClass(`round-${matchInt}`).getElementByClass("player")
-        console.log(value)
+        console.log(this.matches.indexOf(round))
+        let winnerint = this.matches[0].flat().indexOf(playername)
+        let nextmatchint = this.matches.indexOf(round) + 1;
+        if (nextmatchint >= 2) {
+          this.matches[nextmatchint][0].push(playername)
+        } else {
+          if (winnerint > round.length) {
+            this.matches[nextmatchint][1].push(playername)
+          } else {
+            this.matches[nextmatchint][0].push(playername)
+          }
+        }
+        
+        
+        
+        console.log(this.matches)
       },
       pageSwitch(dir) {
-        console.log(dir)
+        //console.log(dir)
         if (dir === 'next') {
           if (this.page = 2) return
+          
           this.page += 1;
         } else if (dir === 'past') {
           if (this.page = 0) return
           this.page -= 1;
         }
+        console.log(this.page)
         
-        this.playerInt = parseInt(this.playerInt);
       },
     },
     mounted() {
@@ -129,6 +158,20 @@ export default {
       
     }
 }
+/*
+matches: [
+  [
+    ['player1', 'player2'],
+    ['player12', 'player22']
+  ],
+  [
+    ['winner1', "winner2"]
+  ],
+  [
+    ['final winner']
+  ]
+]
+*/
 </script>
 
 <style lang="scss">
