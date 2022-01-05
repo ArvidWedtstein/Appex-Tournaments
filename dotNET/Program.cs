61fd7070d3117ebad407c769a988496a6c51c957
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2022-01-05 10:24:51 +0100

    e

---
+++ dotNET/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using tournament;
+
+namespace tournament
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            string port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+            string url = String.Concat("http://0.0.0.0:", port);
+
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>().UseUrls(url);
+                });
+        }
+    }
+}
--- dotNET/Startup.cs
+++ dotNET/Startup.cs
@@ -9,43 +9,58 @@ using Microsoft.OpenApi.Models;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc.Cors;
 using System.Text.Json;
-var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
-// Add services to the container.
-builder.Services.Configure<TournamentDatabaseSettings>(builder.Configuration.GetSection(nameof(TournamentDatabaseSettings)));
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyAllowSpecificOrigins, builder => { builder.WithOrigins("*", "https://localhost:3000").AllowAnyHeader() .AllowAnyMethod(); });
-    
-});
-builder.Services.AddSingleton<ITournamentDatabaseSettings>(sp => sp.GetRequiredService<IOptions<TournamentDatabaseSettings>>().Value);
-builder.Services.AddSingleton<TournamentService>();
-builder.Services.AddControllers();
+namespace tournament {
+    public class Startup {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+            builder.Logging.ClearProviders();
+            builder.Logging.AddConsole();
+            // Add services to the container.
+            builder.Services.Configure<TournamentDatabaseSettings>(builder.Configuration.GetSection(nameof(TournamentDatabaseSettings)));
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins, builder => { builder.WithOrigins("*", "https://localhost:3000").AllowAnyHeader() .AllowAnyMethod(); });
+                
+            });
+            builder.Services.AddSingleton<ITournamentDatabaseSettings>(sp => sp.GetRequiredService<IOptions<TournamentDatabaseSettings>>().Value);
+            builder.Services.AddSingleton<TournamentService>();
+            builder.Services.AddControllers();
 
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-//builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            //builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            
+            app.UseRouting();
+            app.UseCors(MyAllowSpecificOrigins);
+            app.UseHttpsRedirection();
 
-var app = builder.Build();
+            app.UseAuthorization();
 
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+            app.MapControllers();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    var target = Environment.GetEnvironmentVariable("TARGET") ?? "World";
+                    await context.Response.WriteAsync($"Hello {target}!\n");
+                });
+            });
+        }
+    }
 }
-app.UseHttpsRedirection();
-
-app.UseRouting();
-app.UseCors(MyAllowSpecificOrigins);
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
