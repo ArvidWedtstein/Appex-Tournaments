e0e9c5c4e0629358d4a49101b173aefce915f839
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2022-01-12 10:08:11 +0100

    d

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -31,7 +31,12 @@ export default {
     },
     methods: {
       async testrequest() {
-        const tournamentlist = await axios.get(`https://appex-tournaments-gylkpaupva-uc.a.run.app/get-tournament`);
+        const tournamentlist = await axios.get(`https://appex-tournaments-gylkpaupva-uc.a.run.app/get-tournament`, {
+          headers: {
+            'Access-Control-Allow-Origin' : '*',
+            'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+          }
+        });
         console.log(tournamentlist)
       },
       refresh() {
