6abb011b7e030297ed113d29329046fc3345a3da
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2022-01-12 13:51:06 +0100

    s

--- frontend/components/tournamentoverview.vue
+++ frontend/components/tournamentoverview.vue
@@ -52,7 +52,7 @@ export default {
 	},
 	methods: {
 		async matchWin(tournamentId, winner, matchId) {
-      const uri = `https://appex-tournaments-gylkpaupva-uc.a.run.app/Tournament/${tournamentId}?winner=${winner}&matchId=${matchId}`
+      const uri = `https://appex-tournaments-gylkpaupva-uc.a.run.app/Tournament?id=${tournamentId}&winner=${winner}&matchId=${matchId}`
       const res = await axios.post(uri)
       console.log(res)
 			/*await axios({
