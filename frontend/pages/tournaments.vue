d8d6362d54e864380a9d2e08739bd6051ca2ee3e
ArvidW <arvidw0310@gmail.com>
2022-02-02 08:29:59 +0100

    t

--- frontend/pages/tournaments.vue
+++ frontend/pages/tournaments.vue
@@ -105,20 +105,21 @@
         <button class="bg-appexblue text-white rounded py-4 px-8 mx-2 my-2 hover:bg-white border border-transparent font-semibold hover:border-appexblue transition-all duration-100 ease-linear hover:text-appexblue" @click="redigerDeltakere()" type="button">Lagre</button>
       </div>
     </transition>
-
-    <div v-for="(tournament, i) in tournaments" :key="tournament" :id="'tournament' + i" class="tournament rounded" >
-      <button class="absolute rotate-90 top-2.5 right-2.5 w-6 text-center" v-on:click="editTournament(tournament)">✎</button>
-      <div class="absolute bottom-0 left-0 p-5 " @click="showTournament(tournament)">
-        <div class="w-100 text-md pb-0 min-h-100 overflow-auto font-light">
-          <p>Dato: {{formatDate(tournament.date)}}</p>
-        </div>
-        <div class="flex-auto w-100 max-h-8 text-xl font-semibold">
-          <p class="">{{tournament.Name}}</p>
-        </div>
-        <div class="tfooter">
-          <!-- <p class="winner" :v-if="tournament.rounds[tournament.rounds.length - 1][0]">{{tournament.rounds[tournament.rounds.length - 1][0].winner}}</p> -->
-          <p class="players">{{countPlayers(tournament)}}</p>
-          <p v-if="tournament.status" class="status">{{tournament.status}}</p>
+    <div class="container">
+      <div v-for="(tournament, i) in tournaments" :key="tournament" :id="'tournament' + i" class="tournament rounded" >
+        <button class="absolute rotate-90 top-2.5 right-2.5 w-6 text-center" v-on:click="editTournament(tournament)">✎</button>
+        <div class="absolute bottom-0 left-0 p-5 " @click="showTournament(tournament)">
+          <div class="w-100 text-md pb-0 min-h-100 overflow-auto font-light">
+            <p>Dato: {{formatDate(tournament.date)}}</p>
+          </div>
+          <div class="flex-auto w-100 max-h-8 text-xl font-semibold">
+            <p class="">{{tournament.Name}}</p>
+          </div>
+          <div class="tfooter">
+            <!-- <p class="winner" :v-if="tournament.rounds[tournament.rounds.length - 1][0]">{{tournament.rounds[tournament.rounds.length - 1][0].winner}}</p> -->
+            <p class="players">{{countPlayers(tournament)}}</p>
+            <p v-if="tournament.status" class="status">{{tournament.status}}</p>
+          </div>
         </div>
       </div>
     </div>
@@ -336,7 +337,7 @@ html {
       flex: 1 1 auto;
       &::after {
         content: '//';
-        padding: 0 0.5rem;
+        // padding: 0 0.5rem;
       }
     }
   }
