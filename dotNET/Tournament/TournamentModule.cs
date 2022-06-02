05d860ca2e2584e147aea002f95200b60908dbe9
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2022-06-02 18:12:38 +0200

    AAAAAAAAAAA SHIT WORKS

--- dotNET/Tournament/TournamentModule.cs
+++ dotNET/Tournament/TournamentModule.cs
@@ -8,6 +8,8 @@ public class TournamentModule: Module {
     protected override void Load(ContainerBuilder builder)
     {
         builder.RegisterType<TournamentService>().As<ITournamentService>();
+        builder.RegisterType<TournamentRepository>().As<ITournamentRepository>();
+
         builder.Register(c=>new MongoClient(c.Resolve<IOptions<TournamentDatabaseSettings>>().Value.ConnectionString))
             .As<IMongoClient>();
         
